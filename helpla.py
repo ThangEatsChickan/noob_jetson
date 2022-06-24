de65672108d3be59fe729218dd4960d32735bb9c
TheToucanLover <mainameejeff@gmail.com>
2022-06-24 17:28:16 +0800

    new models intown!

--- helpla.py
+++ helpla.py
@@ -4,13 +4,13 @@ import numpy as np
 
 print ("Patching the ONNX model.. ")
 
-graph = gs.import_onnx(onnx.load("savedmodel.onnx"))
+graph = gs.import_onnx(onnx.load("newsavedmodel.onnx"))
 for inp in graph.inputs:
     inp.dtype = np.float32
 
-onnx.save(gs.export_onnx(graph),"updated_savedmodel.onnx")
+onnx.save(gs.export_onnx(graph),"updated_newsavedmodel.onnx")
 
 print ("Check ONNX model using checker function and see if it passes...")
-model = onnx.load("updated_savedmodel.onnx")
+model = onnx.load("updated_newsavedmodel.onnx")
 onnx.checker.check_model(model)
 print('The model is checked!') 
